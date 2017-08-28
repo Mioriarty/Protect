9a5a0a47641bd6498402769d8492e7c4f357527a
Mioriarty <moritz.seppelt@googlemail.com>
2017-08-28 19:30:41 +0200

    Event prototype addded

---
+++ Protect_unity/Assets/EventScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EventScript : MonoBehaviour {
+
+	private GameObject text;
+	private GameObject ps;
+	private Vector2 screenSize;
+	private Vector2 canvasSize;
+
+	void Start () {
+		text = transform.GetChild (0).gameObject;
+		ps = transform.GetChild (1).gameObject;
+
+		ps.transform.parent = null;
+
+		screenSize = new Vector2 (Camera.main.orthographicSize * Screen.width / Screen.height, Camera.main.orthographicSize);
+		canvasSize = transform.root.gameObject.GetComponent<CanvasScaler> ().referenceResolution;
+		Debug.Log (GetComponent<RectTransform>().anchoredPosition.x / canvasSize.x);
+		setPsPos ();
+
+	}
+
+	private void setPsPos(){
+		ps.transform.position = new Vector3 (GetComponent<RectTransform>().anchoredPosition.x / canvasSize.x * screenSize.x, GetComponent<RectTransform>().anchoredPosition.y / canvasSize.y * screenSize.y, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		setPsPos ();
+	}
+
+	void OnDestroy(){
+		Destroy (ps);
+	}	
+
+}
---
+++ Protect_unity/Assets/RemoveBehavior.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveBehavior : StateMachineBehaviour {
+
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+		Debug.Log ("End");
+		Destroy (animator.gameObject);
+	}
+		
+}
